3de8e5f36c44eed923da536e4ebf9eb075bd4926
SeonjaeLee <tjswo9502@gmail.com>
2024-04-07 17:25:08 +0900

    :memo: view 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,23 @@
 import styled from 'styled-components';
-import { Map, InfoSheet, MapProvider, Button, ContextPopup } from '@/components';
+import { Map, InfoSheet, MapContext, Text } from '@/components';
+import { useContext } from 'react';
 
 export default function Home() {
+  const { pointer } = useContext(MapContext);
   return (
-    <MapProvider>
-      <Container>
-        <ContextPopup
-          $contents={
-            <>
-              <Button>Hello</Button>
-              <Button>Bye</Button>
-              <Button>Hi</Button>
-            </>
-          }
-        >
-          <Map />
-        </ContextPopup>
-        <InfoSheet>{/* TODO inner 추가할것  */}</InfoSheet>
-      </Container>
-    </MapProvider>
+    <Container>
+      <Map />
+      <InfoSheet>
+        {pointer ? (
+          <>
+            <Text>{pointer && pointer.coord.x}</Text>
+            <Text>{pointer && pointer.coord.y}</Text>
+          </>
+        ) : (
+          <Text>선택되지않음</Text>
+        )}
+      </InfoSheet>
+    </Container>
   );
 }
 
