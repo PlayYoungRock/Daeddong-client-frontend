2e061a252f8a5d0d84563941c7f18c9c2d76b863
SeonjaeLee <tjswo9502@gmail.com>
2024-04-10 18:33:47 +0900

    :sparkles: 화장실 목록 api

---
+++ src/utils/toiletApi.ts
@@ -0,0 +1,45 @@
+import { httpInstance } from './http';
+
+export const TOILET_LIST = 'toiletList';
+
+export interface ToiletType {
+  seq: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  address: string;
+  si: string;
+  gungu: string;
+  floor: string | null;
+  openTime: string;
+  closeTime: string;
+  regDt: string;
+  modDt: string | null;
+  manageAgency: string;
+  maNum: string | null;
+  toiletType: string;
+  countMan: number;
+  countWomen: number;
+  babyYn: string;
+  unusualYn: number;
+  cctvYn: string;
+  alarmYn: string;
+  pwdYn: string;
+  pwd: string;
+  etc: string;
+  regId: string;
+  modId: string | null;
+  openYn: string;
+}
+
+export const getToiletList = async (params: {
+  distance: number;
+  latitude: number;
+  longitude: number;
+}) => {
+  const { data } = await httpInstance.get<{ data: { toiletList: ToiletType[] } }>(TOILET_LIST, {
+    params: params,
+  });
+
+  return data.data.toiletList;
+};
