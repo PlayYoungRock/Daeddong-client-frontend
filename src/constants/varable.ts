ff2779eb7741a33800030f1a5e9630322e029b4c
SeonjaeLee <tjswo9502@gmail.com>
2024-05-12 01:26:22 +0900

    :sparkles: 모바일환경확인 변수

--- src/constants/varable.ts
+++ src/constants/varable.ts
@@ -1,3 +1,8 @@
 export const layoutId = 'layout';
 export const NoneOption = { label: '없음', value: '' };
 export const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+export const isMobile =
+  isBrowser &&
+  window?.navigator.userAgent.match(
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i,
+  );
